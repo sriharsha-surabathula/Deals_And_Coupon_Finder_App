0731d96860e29325c5713140010331f9cd53c582
sriharsha-surabathula <101316677+sriharsha-surabathula@users.noreply.github.com>
2022-09-04 12:20:13 +0530

    Add files via upload

---
+++ discovery-server/discovery-server/src/test/java/io/javabrains/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,15 @@
+package io.javabrains.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+public class DiscoveryServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
